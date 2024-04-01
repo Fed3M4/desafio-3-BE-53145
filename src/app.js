67ace8b7f8b01fd32c3d8b96e42641d4c7ca1a34
Federico <nunez.fedema@gmmail.com>
2024-04-01 15:56:38 -0300

    creacion del servidor con peticiones get

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import ProductManager from './desafio.js'
+
+const app = express()
+const port = 8080
+const pm = new ProductManager('./data.json')
+
+app.use(express.urlencoded({extended:true}))
+
+
+app.listen(port, () => console.log(`¡Servidor arriba en el puerto ${port}`))
+
+app.get('/bienvenida', (req, res) => {
+    res.send('<h1>Bienvenido</h1>')
+})
+
+app.get('/products', async (req, res) =>{
+    let products = await pm.getProducts()
+    const limit = req.query.limit
+    if(!limit) return res.send(products)
+    res.send(products.slice(0, limit))
+})
+
+app.get('/products/:pid', async(req, res) => {
+    const id = parseInt(req.params.pid)
+    const productById = await pm.getProductById(id)
+    res.send(productById)
+})
